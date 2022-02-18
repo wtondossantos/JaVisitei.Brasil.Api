756f93b272487168e09c3667f62ac5374ea72791
wtondossantos <wellington@wton.com.br>
2022-02-18 07:23:16 -0300

    update test

---
+++ JaVisitei.Brasil.Test/UsuariosControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JaVisitei.Brasil.Test
+{
+    [TestClass]
+    public class UsuariosTest
+    {
+        public UsuariosTest()
+        {
+        }
+
+        [TestMethod]
+        public void PesquisarUsuarioUsername_IsValid_Usuario()
+        {
+            //Assert
+            Assert.IsTrue(true);
+        }
+
+    }
+}
